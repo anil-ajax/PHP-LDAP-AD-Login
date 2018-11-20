03b4da23bd09c08188ac61ee66b2f8e376c4dcfd
Anil Kumar <anil.ajax@gmail.com>
2018-11-20 12:07:18 +0530

    Add files via upload

---
+++ LDAP.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Active Directory(AD) authentication using PHP LDAP
+ * Make sure you have mod_ldap enabled in apache config
+ * @author Anil Kumar (https://github.com/anil-ajax)
+ * 
+ */
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+
+    $ad_server = "ldap://mydomain.com"; // ask your networking team for this
+	
+    $ldap = ldap_connect($ad_server, 389); // 389 is LDAP port, change as per your configurations
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $ldaprdn = 'local_domain' . "\\" . $username;  // ask your networking team for your "local_domain"
+
+    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+
+    $bind = @ldap_bind($ldap, $ldaprdn, $password);
+
+
+    if ($bind) {
+        $filter="(sAMAccountName=$username)";
+        $result = ldap_search($ldap,"dc=MYDOMAIN,dc=COM",$filter);
+        ldap_sort($ldap,$result,"sn");
+        $info = ldap_get_entries($ldap, $result);
+        var_dump($info);
+    }
+}
